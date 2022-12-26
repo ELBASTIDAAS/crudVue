f00a092102cd0285ac86e0d6f8ac7685ac361a92
Miguel Antonio Bastidas Verdin <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-12-26 14:34:19 -0800

    Update ContactManager.vue

--- vue-contact-manager/src/views/ContactManager.vue
+++ vue-contact-manager/src/views/ContactManager.vue
@@ -5,7 +5,7 @@
                 <p class="h3 text-light fw-bold">
                     Contact Manager
                     <router-link to="/contacts/add" class="btn btn-light btn-sm"><i
-                            class="fa fa-plus-circle"></i>New</router-link>
+                            class="fa fa-plus-circle"></i> New</router-link>
                 </p>
                 <p class="text-light fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
                     doloribus maxime doloremque suscipit error enim voluptas, ratione autem placeat molestiae libero
