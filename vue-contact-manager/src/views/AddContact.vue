1a5ab078e2fa39faff50132a60c809e08020e7e0
Miguel Antonio Bastidas Verdin <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-12-26 14:29:22 -0800

    session 2 edit

--- vue-contact-manager/src/views/AddContact.vue
+++ vue-contact-manager/src/views/AddContact.vue
@@ -11,28 +11,29 @@
     <div class="container mt-3">
         <div class="row">
             <div class="col-md-4">
-                <form>
+                <form @submit.prevent="submitCreate()">
                     <div class="mb-2">
-                        <input type="text" class="form-control" placeholder="Name">
+                        <input v-model="contact.name" type="text" class="form-control" placeholder="Name">
                     </div>
                     <div class="mb-2">
-                        <input type="text" class="form-control" placeholder="Photo URL">
+                        <input v-model="contact.photo" type="text" class="form-control" placeholder="Photo URL">
                     </div>
                     <div class="mb-2">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input v-model="contact.email" type="email" class="form-control" placeholder="Email">
                     </div>
                     <div class="mb-2">
-                        <input type="number" class="form-control" placeholder="Mobile">
+                        <input v-model="contact.mobile" type="number" class="form-control" placeholder="Mobile">
                     </div>
                     <div class="mb-2">
-                        <input type="text" class="form-control" placeholder="Company">
+                        <input v-model="contact.company" type="text" class="form-control" placeholder="Company">
                     </div>
                     <div class="mb-2">
-                        <input type="text" class="form-control" placeholder="Title">
+                        <input v-model="contact.title" type="text" class="form-control" placeholder="Title">
                     </div>
                     <div class="mb-2">
-                        <select class="form-control">
+                        <select v-model="contact.groupId" class="form-control" v-if="groups.length > 0">
                             <option value="">Select Group</option>
+                            <option :value="group.id" v-for="group in groups" :key="group.id">{{group.name}}</option>
                         </select>
                     </div>
                     <div class="mb-2">
@@ -41,15 +42,55 @@
                 </form>
             </div>
             <div class="col-md-4">
-                <img src="https://cdn-icons-png.flaticon.com/512/219/219969.png" class="contact-img">
+                <img :src="contact.photo" class="contact-img">
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { ContactService } from '@/services/ContactService';
 export default {
-    name: 'addContact'
+    name: 'addContact',
+    data : function() {
+        return {
+            contact : {
+                name: '',
+                photo: '',
+                email: '',
+                mobile: '',
+                company: '',
+                title: '',
+                group: '',
+                groupId: '',
+            },
+            groups: []
+            
+        }
+    },
+    created : async function() {
+        try {
+            let response = await ContactService.getAllGroups();
+            this.groups = response.data;
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    methods: {
+        submitCreate: async function() {
+            try {
+                let response = await ContactService.createContact(this.contact);
+                if (response) {
+                    return this.$router.push('/')
+                }else{
+                    return this.$router.push('/contacts/add')
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+    
 }
 </script>
 
